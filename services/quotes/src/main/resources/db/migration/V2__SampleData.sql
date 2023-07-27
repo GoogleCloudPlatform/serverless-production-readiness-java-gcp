cf2c17f6a59740689d4c1514b58a22ba91f459e3
ddobrin <ddobrin@google.com>
2023-07-27 10:36:30 -0400

    added copyright where missing

--- services/quotes/src/main/resources/db/migration/V2__SampleData.sql
+++ services/quotes/src/main/resources/db/migration/V2__SampleData.sql
@@ -1,3 +1,18 @@
+/*
+* Copyright 2023 Google LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 INSERT INTO quotes (quote,author, book) VALUES ('Who controls the past controls the future. Who controls the present controls the past.','George Orwell','1984');
 INSERT INTO quotes (quote,author, book) VALUES ('You forget what you want to remember, and you remember what you want to forget.','Cormac McCarthy', 'The Road');
 INSERT INTO quotes (quote,author, book) VALUES ('All we can know is that we know nothing. And that''s the height of human wisdom.','Leo Tolstoy', 'War and Peace');
