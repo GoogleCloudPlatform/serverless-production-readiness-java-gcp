dbfa3ebf51df19addb5d6006eaa9497f07d7717f
ddobrin <ddobrin@google.com>
2023-11-07 09:53:27 -0500

    Updated scripts

--- runtimes/crac/quotes-crac/checkpoint.sh
+++ runtimes/crac/quotes-crac/checkpoint.sh
@@ -31,7 +31,7 @@ echo "Using CRaC enabled JDK $url"
 
 ./mvnw clean package
 docker build -t quotes-crac:builder --build-arg CRAC_JDK_URL=$url .
-docker run -d --privileged --rm --name=quotes-crac --ulimit nofile=1024 -p 8080:8083 -v $(pwd)/target:/opt/mnt -e FLAG=$1 quotes-crac:builder
+docker run -d --privileged --rm --name=quotes-crac --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt -e FLAG=$1 quotes-crac:builder
 echo "Please wait during checkpoint creation..."
 sleep 10
 docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=quotes-crac") quotes-crac:checkpoint
