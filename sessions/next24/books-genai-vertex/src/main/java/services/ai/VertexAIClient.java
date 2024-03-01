ddd010ffb9591cebc25086883bf77ae4ae122db0
yannipeng <yannipeng@google.com>
2024-03-01 12:10:32 -0800

    added logic for VertexAIClient for different model api calls

--- sessions/next24/books-genai-vertex/src/main/java/services/ai/VertexAIClient.java
+++ sessions/next24/books-genai-vertex/src/main/java/services/ai/VertexAIClient.java
@@ -4,6 +4,7 @@ import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.output.Response;
 import dev.langchain4j.model.vertexai.VertexAiChatModel;
+import dev.langchain4j.model.vertexai.VertexAiLanguageModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,24 +15,44 @@ import services.web.ImageProcessingController;
 public class VertexAIClient {
     private static final Logger logger = LoggerFactory.getLogger(VertexAIClient.class);
     public String prompt(String prompt, String modelName) {
-        VertexAiChatModel vertexAiChatModel = VertexAiChatModel.builder()
-                .endpoint("us-central1-aiplatform.googleapis.com:443")
-                .project(CloudConfig.projectID)
-                .location(CloudConfig.zone)
-                .publisher("google")
-                .modelName(modelName)
-                .temperature(0.1)
-                .maxOutputTokens(1000)
-                .topK(0)
-                .topP(0.0)
-                .maxRetries(3)
-                .build();
+        String output =null;
+        if (modelName.contains("chat")) {
+            VertexAiChatModel vertexAiChatModel = VertexAiChatModel.builder()
+                    .endpoint("us-central1-aiplatform.googleapis.com:443")
+                    .project(CloudConfig.projectID)
+                    .location(CloudConfig.zone)
+                    .publisher("google")
+                    .modelName(modelName)
+                    .temperature(0.1)
+                    .maxOutputTokens(500)
+                    .topK(0)
+                    .topP(0.0)
+                    .maxRetries(3)
+                    .build();
+            Response<AiMessage> modelResponse = vertexAiChatModel.generate(UserMessage.from(prompt));
+            output = modelResponse.content().text();
+            logger.info("Result Chat Model: " + modelResponse.content().text());
+        } else {
+            VertexAiLanguageModel vertexAiTextModel = VertexAiLanguageModel.builder()
+                    .endpoint("us-central1-aiplatform.googleapis.com:443")
+                    .project(CloudConfig.projectID)
+                    .location(CloudConfig.zone)
+                    .publisher("google")
+                    .modelName(modelName)
+                    .temperature(0.1)
+                    .maxOutputTokens(50)
+                    .topK(0)
+                    .topP(0.0)
+                    .maxRetries(3)
+                    .build();
+            Response<String> textResponse = vertexAiTextModel.generate(prompt);
+            output = textResponse.content();
+        }
+
 
         logger.info("The prompt & model name are: " + prompt +" | "+modelName);
-
-
-        Response<AiMessage> modelResponse = vertexAiChatModel.generate(UserMessage.from(prompt));
-        return modelResponse.content().text();
+        logger.info(output);
+        return output;
     }
 
 }
