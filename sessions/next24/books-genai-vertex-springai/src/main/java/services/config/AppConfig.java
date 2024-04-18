17d80cf7bcad67974f9666d5627c2d9596ae0e42
yannipeng <yannipeng@google.com>
2024-04-17 21:33:34 -0700

    remove unwanted imports

--- sessions/next24/books-genai-vertex-springai/src/main/java/services/config/AppConfig.java
+++ sessions/next24/books-genai-vertex-springai/src/main/java/services/config/AppConfig.java
@@ -18,7 +18,6 @@ package services.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.env.Environment;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
