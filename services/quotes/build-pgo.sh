37663b290b2a6067455c0b5e438556466c4d2675
Dan Dobrin <ddobrin@google.com>
2024-05-17 21:48:37 +0000

    updated /runtimes folder for CRaC based services

--- services/quotes/build-pgo.sh
+++ services/quotes/build-pgo.sh
@@ -1,5 +1,9 @@
 # Build Docker image for Native Image with PGO
 
+# how to build the PGO image
+#   ./mvnw -Pnative -DbuildArgs=--pgo-instrument native:compile -f pom-3.2.x.xml 
+#./mvnw -Pnative -DbuildArgs=--pgo=default.iprof native:compile -f pom-3.2.x.xml -DskipTests
+
 docker rmi quotes-pgo:latest
 docker rmi europe-docker.pkg.dev/spring-io/quotes-native/quotes-pgo:latest
 
