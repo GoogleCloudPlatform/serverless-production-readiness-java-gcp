5bbf82fc84b83f6ae7a4f5792c8829fd0d84e8e1
Alok Srivastava <alok.leo06@gmail.com>
2025-03-20 18:15:35 +0530

    push to testjavanew10

--- services/quotes-ui/src/main/java/com/example/quotes/web/QuoteEndpoint.java
+++ services/quotes-ui/src/main/java/com/example/quotes/web/QuoteEndpoint.java
@@ -90,12 +90,4 @@ public class QuoteEndpoint implements CrudService<Quote, Long> {
         return quoteService.list(pageable, jpaFilterConverter.toSpec(filter, Quote.class));
     }
 }
-#Generate error    @Override
-    public long count(@Nullable Filter filter) {
-        return quoteService.count(jpaFilterConverter.toSpec(filter, Quote.class));
-    }
 
-    @Override
-    public Quote findById(Long id) {
-        return quoteService.findById(id).orElse(null);
-    }
