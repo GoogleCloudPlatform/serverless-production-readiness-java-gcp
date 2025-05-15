7026ac2d4f8d0ea0976c862432860898520c16c3
Jason De Lorme <jasondel@google.com>
2025-05-15 11:25:18 -0400

    Fixing CORS error.

--- services/quotes/src/main/java/com/example/quotes/web/QuoteController.java
+++ services/quotes/src/main/java/com/example/quotes/web/QuoteController.java
@@ -28,6 +28,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,7 @@ import java.util.Date;
  * Controller to handle requests for Quote CRUD operations
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class QuoteController {
     private static final Logger logger = LoggerFactory.getLogger(QuoteController.class);
 
